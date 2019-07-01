43acf1927fab9f524ef128beaa916ad71f3ba84c
madelineliao <madeline.m.liao@gmail.com>
2019-07-01 16:12:42 -0700

    changed hello world to warning message, added current time extension

--- helloworld/out/extension.js
+++ helloworld/out/extension.js
@@ -12,12 +12,18 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+    let disposable1 = vscode.commands.registerCommand('extension.helloWorld', () => {
         // The code you place here will be executed every time your command is executed
         // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showWarningMessage('Hello World!');
     });
-    context.subscriptions.push(disposable);
+    let disposable2 = vscode.commands.registerCommand('extension.currTime', () => {
+        let currTime = Date();
+        // Display a message box with current date/time
+        vscode.window.showInformationMessage(currTime);
+    });
+    context.subscriptions.push(disposable1);
+    context.subscriptions.push(disposable2);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
