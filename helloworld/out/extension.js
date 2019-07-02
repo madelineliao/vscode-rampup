14d915e3160efde45399317eef3f157621c8ff90
madelineliao <madeline.m.liao@gmail.com>
2019-07-02 16:30:20 -0700

    new view now displays the kubernetes clusters of the user (with some bugs)

--- helloworld/out/extension.js
+++ helloworld/out/extension.js
@@ -1,8 +1,31 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
+const fs_1 = require("./fs");
+const host_1 = require("./host");
+const explorer = require("./explorer");
+const shell_1 = require("./shell");
+const config = require("./components/config/config");
+const kubectl_1 = require("./kubectl");
+const minikube_1 = require("./components/clusterprovider/minikube/minikube");
+const draft_1 = require("./draft/draft");
+const installer_1 = require("./components/installer/installer");
+const errorable_1 = require("./errorable");
+const kubeChannel_1 = require("./kubeChannel");
+const helmexec = require("./helm.exec");
+const minikube = minikube_1.create(host_1.host, fs_1.fs, shell_1.shell, installDependencies);
+const draft = draft_1.create(host_1.host, fs_1.fs, shell_1.shell, installDependencies);
+const kubectl = kubectl_1.create(config.getKubectlVersioning(), host_1.host, fs_1.fs, shell_1.shell, installDependencies);
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
@@ -24,9 +47,48 @@ function activate(context) {
     });
     context.subscriptions.push(disposable1);
     context.subscriptions.push(disposable2);
+    const treeProvider = explorer.create(kubectl, host_1.host);
+    vscode.window.registerTreeDataProvider('extension.vsKubernetesExplorer', treeProvider);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
 function deactivate() { }
 exports.deactivate = deactivate;
+function installDependencies() {
+    return __awaiter(this, void 0, void 0, function* () {
+        // TODO: gosh our binchecking is untidy
+        const gotKubectl = yield kubectl.checkPresent(kubectl_1.CheckPresentMessageMode.Silent);
+        const gotHelm = helmexec.ensureHelm(helmexec.EnsureMode.Silent);
+        const gotDraft = yield draft.checkPresent(draft_1.CheckPresentMode.Silent);
+        const gotMinikube = yield minikube.checkPresent(minikube_1.CheckPresentMode.Silent);
+        const installPromises = [
+            installDependency("kubectl", gotKubectl, installer_1.installKubectl),
+            installDependency("Helm", gotHelm, installer_1.installHelm),
+            installDependency("Draft", gotDraft, installer_1.installDraft),
+        ];
+        if (!config.getUseWsl()) {
+            // TODO: Install Win32 Minikube
+            installPromises.push(installDependency("Minikube", gotMinikube, (shell) => {
+                return installer_1.installMinikube(shell, null);
+            }));
+        }
+        yield Promise.all(installPromises);
+        kubeChannel_1.kubeChannel.showOutput("Done");
+    });
+}
+exports.installDependencies = installDependencies;
+function installDependency(name, alreadyGot, installFunc) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (alreadyGot) {
+            kubeChannel_1.kubeChannel.showOutput(`Already got ${name}...`);
+        }
+        else {
+            kubeChannel_1.kubeChannel.showOutput(`Installing ${name}...`);
+            const result = yield installFunc(shell_1.shell);
+            if (errorable_1.failed(result)) {
+                kubeChannel_1.kubeChannel.showOutput(`Unable to install ${name}: ${result.error[0]}`);
+            }
+        }
+    });
+}
 //# sourceMappingURL=extension.js.map
--- helloworld/src/extension.ts
+++ helloworld/src/extension.ts
@@ -1,6 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { fs } from './fs';
+
+import { host } from './host';
+import * as explorer from './explorer';
+import { shell, Shell, ShellResult, ShellHandler } from './shell';
+import * as config from './components/config/config';
+import { CheckPresentMessageMode, create as kubectlCreate } from './kubectl';
+import { create as minikubeCreate, CheckPresentMode as MinikubeCheckPresentMode } from './components/clusterprovider/minikube/minikube';
+import { create as draftCreate, CheckPresentMode as DraftCheckPresentMode } from './draft/draft';
+import { installHelm, installDraft, installKubectl, installMinikube } from './components/installer/installer';
+import { Errorable, failed, succeeded } from './errorable';
+
+import { kubeChannel } from './kubeChannel';
+
+import * as helmexec from './helm.exec';
+import * as helm from './helm';
+
+const minikube = minikubeCreate(host, fs, shell, installDependencies);
+const draft = draftCreate(host, fs, shell, installDependencies);
+const kubectl = kubectlCreate(config.getKubectlVersioning(), host, fs, shell, installDependencies);
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -21,7 +42,7 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	let disposable2 = vscode.commands.registerCommand('extension.currTime', () => {
-		let currTime = Date()
+		let currTime = Date();
 
 		// Display a message box with current date/time
 		vscode.window.showInformationMessage(currTime);
@@ -29,7 +50,47 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(disposable1);
 	context.subscriptions.push(disposable2);
+
+	const treeProvider = explorer.create(kubectl, host);
+
+	vscode.window.registerTreeDataProvider('extension.vsKubernetesExplorer', treeProvider);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+export async function installDependencies() {
+    // TODO: gosh our binchecking is untidy
+    const gotKubectl = await kubectl.checkPresent(CheckPresentMessageMode.Silent);
+    const gotHelm = helmexec.ensureHelm(helmexec.EnsureMode.Silent);
+    const gotDraft = await draft.checkPresent(DraftCheckPresentMode.Silent);
+	const gotMinikube = await minikube.checkPresent(MinikubeCheckPresentMode.Silent);
+	
+    const installPromises = [
+        installDependency("kubectl", gotKubectl, installKubectl),
+        installDependency("Helm", gotHelm, installHelm),
+        installDependency("Draft", gotDraft, installDraft),
+    ];
+
+    if (!config.getUseWsl()) {
+        // TODO: Install Win32 Minikube
+        installPromises.push(
+            installDependency("Minikube", gotMinikube, (shell: Shell) => {
+                return installMinikube(shell, null);
+            }));
+    }
+    await Promise.all(installPromises);
+
+    kubeChannel.showOutput("Done");
+}
+
+async function installDependency(name: string, alreadyGot: boolean, installFunc: (shell: Shell) => Promise<Errorable<null>>): Promise<void> {
+    if (alreadyGot) {
+        kubeChannel.showOutput(`Already got ${name}...`);
+    } else {
+        kubeChannel.showOutput(`Installing ${name}...`);
+        const result = await installFunc(shell);
+        if (failed(result)) {
+            kubeChannel.showOutput(`Unable to install ${name}: ${result.error[0]}`);
+        }
+    }
+}
